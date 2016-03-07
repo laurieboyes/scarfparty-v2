32820fed03b13484e6ec621f5fcb86964ee5ad39
Laurie <laurie.boyes@gmail.com>
2016-03-07 22:59:08 +0000

    automated browser testing with mocha-phantomjs

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,5 @@
 import Pattern from '../src/pattern.js'
 
-// Run this first:
-//  obt build --js=test/tests.js --buildJs=tests-compiled.js
-// todo automate
-
 describe('Extracting the pattern from an image', function() {
     it('Should get a sweet sweet pattern from the test image', function(done) {
 
