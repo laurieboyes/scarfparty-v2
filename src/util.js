b95f6c59bec56a66e4867350144141e202540c38
Laurie Boyes <laurie.boyes@ft.com>
2016-03-13 11:11:36 +0000

    removed some accidental rubbish code

--- src/util.js
+++ src/util.js
@@ -5,15 +5,3 @@ export function ready(fn) {
         document.addEventListener('DOMContentLoaded', fn);
     }
 }
-
-export function debounce(fn) {
-		$('input').keypress(function() {
-			if (this.timeoutId)
-				window.clearTimeout(this.timeoutId);
-			this.timeoutId = window.setTimeout(function () {
-				$.ajax({
-					// do some stuff
-				});
-			}, 200);
-		});
-}
